2c49b9ebd189f3df0f2d69fbc7f0c5d51b99390b
nelliewu95 <yw348@cornell.edu>
2019-10-03 18:21:45 -0400

    accelerate processing - fix the idle energy too large problem - accelerate energy estimations by reusing data - use bash to call CACTI - used bash saved to /tmp/ - more clear action type specifications

--- cacti_wrapper.py
+++ cacti_wrapper.py
@@ -3,7 +3,7 @@ CACTI_ACCURACY = 70  # in your metric, please set the accuracy you think CACTI's
 #-------------------------------------------------------------------------------
 # CACTI7 wrapper for generating energy estimations for plain SRAM scraptchpad
 #-------------------------------------------------------------------------------
-import subprocess, os, csv, glob
+import subprocess, os, csv, glob, tempfile, math
 
 class CactiWrapper:
     """
@@ -17,6 +17,7 @@ class CactiWrapper:
 
         # example primitive classes supported by this estimator
         self.supported_pc = ['SRAM']
+        self.energy_records = {} # enable data reuse
 
     def primitive_action_supported(self, interface):
         """
@@ -67,15 +68,15 @@ class CactiWrapper:
         return energy
 
     def search_for_cacti_exec(self):
-
         # search the current directory first, top-down walk
         this_dir, this_filename = os.path.split(__file__)
         for root, directories, file_names in os.walk(this_dir):
-            for file_name in file_names:
-                if file_name == 'cacti':
-                    cacti_exec_path = root + os.sep + file_name
-                    cacti_exec_dir = os.path.dirname(cacti_exec_path)
-                    return cacti_exec_dir
+            if 'obj_dbg' not in root:
+                for file_name in file_names:
+                    if file_name == 'cacti':
+                        cacti_exec_path = root + os.sep + file_name
+                        cacti_exec_dir = os.path.dirname(cacti_exec_path)
+                        return cacti_exec_dir
 
         # search the PATH variable: search the directories provided in the PATH variable. top-down walk
         PATH_lst = os.environ['PATH'].split(os.pathsep)
@@ -88,7 +89,6 @@ class CactiWrapper:
                         return cacti_exec_dir
 
     def SRAM_estimate_energy(self, interface):
-
         # translate the attribute names into the ones that can be understood by Cacti
         attributes = interface['attributes']
         tech_node = attributes['technology']
@@ -98,55 +98,55 @@ class CactiWrapper:
         wordsize_in_bytes = attributes['width'] // 8
         n_rw_ports = attributes['n_rdwr_ports'] + attributes['n_rd_ports'] + attributes['n_wr_ports']
         n_banks = attributes['n_banks']
-
-        curr_dir = os.path.abspath(os.getcwd())
-        cacti_exec_dir = self.search_for_cacti_exec()
-        os.chdir(cacti_exec_dir)
-        # check if the generated data already covers the case
-
-        action_name = interface['action_name']
-        if action_name == 'read':
-            cacti_entry = ' Dynamic read energy (nJ)'   # nJ
-        elif action_name =='write':
-            cacti_entry = ' Dynamic write energy (nJ)'  # nJ
-        else:
-            cacti_entry = ' Standby leakage per bank(mW)' # mW
-
-        csv_file_path = 'out.csv'
-        #============================================================================
-        # Optimization for data reuse, uncomment if want to use
-        # need to modify cacti output csv entries to match the required field names
-        # 1. Number of banks need to be added as a field to output csv as a field
-        # 2. Number of rw ports need to be added as a field to output csv as a field
-        #============================================================================
-        # if os.path.exists(csv_file_path):
-        #     with open(csv_file_path) as csv_file:
-        #         reader = csv.DictReader(csv_file)
-        #         for row in reader:
-        #             if  int(row[' Output width (bits)']) == attributes['width']\
-        #                 and int(row['Tech node (nm)']) == int(tech_node) \
-        #                 and int(row[' Capacity (bytes)']) == size_in_bytes \
-        #                 and int(row[' Number of banks']) == n_banks \
-        #                 and int(row[' Number of rw ports']) == n_rw_ports:
-        #                 if not action_name == 'idle':
-        #                     energy = float(row[cacti_entry])
-        #                 else:
-        #                     energy = float(row[cacti_entry]) * 0.001 * float(row['Random cycle time (ns)'])
-        #                 os.chdir(curr_dir)
-        #                 return energy  # found the data in the csv file
-
-        # query Cacti
-        self.cacti_wrapper_for_SRAM(cacti_exec_dir, tech_node, size_in_bytes, wordsize_in_bytes, n_rw_ports, n_banks)
-        with open(csv_file_path) as csv_file:
-            reader = csv.DictReader(csv_file)
-            row = list(reader)[-1]
-            if not action_name == 'idle':
-                energy = float(row[cacti_entry]) * 1000 # original energy is in has nJ as the unit
+        desired_action_name = interface['action_name']
+        desired_entry_key = (desired_action_name, tech_node, size_in_bytes, wordsize_in_bytes, n_rw_ports, n_banks)
+        if desired_entry_key in self.energy_records:
+            if desired_action_name == 'idle':
+                energy = self.energy_records[desired_entry_key]
             else:
-                standby_power_in_pw = float(row[cacti_entry]) * 1000000000 # mW -> pW
-                idle_energy_per_bank_in_pj = standby_power_in_pw * float(row[' Random cycle time (ns)']) * 1000
-                energy = idle_energy_per_bank_in_pj * n_banks
-        os.chdir(curr_dir)
+                address_delta = interface['arguments']['address_delta']
+                data_delta = interface['arguments']['data_delta']
+                if address_delta == 0 and data_delta == 0:
+                    interpreted_entry_key = ('idle', tech_node, size_in_bytes, wordsize_in_bytes, n_rw_ports, n_banks)
+                    energy = self.energy_records[interpreted_entry_key]
+                else:
+                    interpreted_entry_key = ('idle', tech_node, size_in_bytes, wordsize_in_bytes, n_rw_ports, n_banks)
+                    idle_banks_energy = self.energy_records[interpreted_entry_key] * (n_banks- address_delta) / n_banks
+                    active_banks_energy = self.energy_records[desired_entry_key] * address_delta / n_banks
+                    energy = idle_banks_energy + active_banks_energy
+        else:
+            print('Info: CACTI plug-in... Querying CACTI for query:\n', interface)
+            curr_dir = os.path.abspath(os.getcwd())
+            cacti_exec_dir = self.search_for_cacti_exec()
+            os.chdir(cacti_exec_dir)
+            # check if the generated data already covers the case
+            self.cacti_wrapper_for_SRAM(cacti_exec_dir, tech_node, size_in_bytes, wordsize_in_bytes, n_rw_ports,
+                                        n_banks, interface)
+            for action_name in ['read', 'write', 'idle']:
+                entry_key = (action_name, tech_node, size_in_bytes, wordsize_in_bytes, n_rw_ports, n_banks)
+                if action_name == 'read':
+                    cacti_entry = ' Dynamic read energy (nJ)'   # nJ
+                elif action_name =='write':
+                    cacti_entry = ' Dynamic write energy (nJ)'  # nJ
+                else:
+                    cacti_entry = ' Standby leakage per bank(mW)' # mW
+                csv_file_path = cacti_exec_dir + '/out.csv'
+                # query Cacti
+                with open(csv_file_path) as csv_file:
+                    reader = csv.DictReader(csv_file)
+                    row = list(reader)[-1]
+                    if not action_name == 'idle':
+                        energy = float(row[cacti_entry]) * 10**3# original energy is in has nJ as the unit
+                    else:
+                        standby_power_in_w = float(row[cacti_entry]) * 10**-3 # mW -> W
+                        idle_energy_per_bank_in_j = standby_power_in_w * float(row[' Random cycle time (ns)']) * 10**-9
+                        idle_energy_per_bank_in_pj = idle_energy_per_bank_in_j * 10**12
+                        energy = idle_energy_per_bank_in_pj * n_banks
+                # record new entry
+                self.energy_records.update({entry_key: energy})
+            energy = self.energy_records[desired_entry_key]
+            os.remove(csv_file_path) # all information recorded, no need for saving the file
+            os.chdir(curr_dir)
         return  energy  # output energy is pJ
 
     def SRAM_attr_supported(self, attributes):
@@ -155,7 +155,7 @@ class CactiWrapper:
             tech_node = tech_node[:-2]  # remove the unit
         size_in_bytes = attributes['width'] * attributes['depth'] // 8
         if size_in_bytes < 64:
-            return False  # Cacti only estimates energy for SRAM size larger than 64B
+            return False  # Cacti only estimates energy for SRAM size larger than 64B (512b)
         if int(tech_node) < 22 or int(tech_node) > 180:
             return False  # Cacti only supports technology that is between 22nm to 180 nm
         return True
@@ -168,23 +168,27 @@ class CactiWrapper:
         else:
             return None
 
-    def cacti_wrapper_for_SRAM(self, cacti_exec_dir, tech_node, size_in_bytes, wordsize_in_bytes, n_rw_ports, n_banks):
+    def cacti_wrapper_for_SRAM(self, cacti_exec_dir, tech_node, size_in_bytes, wordsize_in_bytes, n_rw_ports, n_banks, original_request):
         tech_node = tech_node  # technology node described in nm
         cache_size = size_in_bytes
         line_size = wordsize_in_bytes
         if int(wordsize_in_bytes) < 4:  # minimum line size in cacti is 32-bit/4-byte
             line_size = 4
         if int(cache_size) / int(line_size) < 64:
-            print('WARN: intended cache size is smaller than 64 words')
+            print('WARN: CACTI Plug-in...  intended cache size is smaller than 64 words')
             print('intended cache size:', cache_size, 'line size:', line_size)
             cache_size = int(line_size) * 64  # minimum scratchpad size: 64 words
             print('corrected cache size:', cache_size)
+        if not math.ceil(math.log2(n_banks)) == math.floor(math.log2(n_banks)):
+            print('WARN: CACTI Plug-in... "n_banks: %s" is not a power of 2'%(str(n_banks)))
+            n_banks = 2**(math.floor(math.log2(n_banks)))
+            print('corrected "n_banks": ', n_banks)
         associativity = 1  # plain scratchpad is a direct mapped cache
         rw_ports = n_rw_ports  # assumes that all the ports in the plain scratchpad are read wrtie ports instead of exclusive ports
         if int(rw_ports) == 0:
             rw_ports = 1  # you must have at least one port
 
-        banks = n_banks  # number of banks you want to divide your srachpad into, dfault is one
+        banks = n_banks  # number of banks you want to divide your scratchpad into, default is one
         excl_read_ports = 0  # assumes no exclusive ports of any type
         excl_write_ports = 0
         single_ended_read_ports = 0
@@ -207,8 +211,8 @@ class CactiWrapper:
 
         # assign weights for CACTI optimizations
         obj_func_delay = 0
-        obj_func_dynamic_power = 100  # stress the importance of dynamic power and leakage power
-        obj_func_leakage_power = 100
+        obj_func_dynamic_power = 0
+        obj_func_leakage_power = 1000
         obj_func_area = 0
         obj_func_cycle_time = 0
 
@@ -247,10 +251,10 @@ class CactiWrapper:
         # create a temporary output file to redirect terminal output of cacti
         if os.path.isfile(cacti_exec_dir + 'tmp_output.txt'):
             os.remove(cacti_exec_dir + 'tmp_output.txt')
-        temp_output =  open("tmp_output.txt", "wb")
+        temp_output =  tempfile.mkstemp()[0]
         # call cacti executable to evaluate energy consumption
         cacti_exec_path = cacti_exec_dir + '/cacti'
-        subprocess.call([cacti_exec_path,
+        exec_list = [cacti_exec_path,
                          str(cache_size),
                          str(line_size),
                          str(associativity),
@@ -302,5 +306,22 @@ class CactiWrapper:
                          str(ndcm),
                          str(ndsam1),
                          str(ndsam2),
-                         str(ecc)], stdout=temp_output)
+                         str(ecc)]
+        temp_dir = tempfile.gettempdir()
+        script_path = temp_dir + '/accelergy_cacti_temp.sh'
+        print('Info: CACTI plug-in... Command line input saved to: ', script_path)
+        f = open(script_path, 'a+')
+        if len(f.readlines()) > 1000:
+            print('WARN:  CACTI Plug-in... temp logs at: ', script_path, 'exceeds 1000 lines, delete file and create new one')
+            os.remove(script_path)
+            f = open(script_path, 'a+')
+        f.write('\n ------------------------- ')
+        f.write('Original Request: \n ' + str(original_request) + '\n')
+        f.write(str())
+        for i in exec_list:
+            f.write(i + ' ')
+        f.close()
+        os.chmod(script_path, 0o775)
+        subprocess.call(exec_list, stdout=temp_output)
+
 
